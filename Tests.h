110f068c6c27bb30796944c8a805cefe34c2b35d
Pankrat <99604167+velocharaptor@users.noreply.github.com>
2022-05-27 23:05:25 +0300

    Update Tests.h

--- Tests.h
+++ Tests.h
@@ -1,6 +1,12 @@
 #ifndef LR3_TESTS_H
 #define LR3_TESTS_H
+#include "Complex.h"
 
 void Tests();
+
+int F1Int1(int x);
+cmp F1Int2(cmp x);
+bool F2Otr(int x);
+bool F2Sr(cmp x);
 
 #endif //LR3_TESTS_H
